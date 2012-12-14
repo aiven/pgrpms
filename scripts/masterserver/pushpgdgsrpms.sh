3752f843db71ab8df0c891a7a4890ca3c088d420
Devrim Gündüz <devrim@gunduz.org>
2012-12-14 10:38:27 +0000

    Sync scripts with the ones that are on the machines

--- scripts/masterserver/pushpgdgsrpms.sh
+++ scripts/masterserver/pushpgdgsrpms.sh
@@ -9,30 +9,29 @@
 cd /media/disk/RPMS/srpms-web/
 
 # If you want to push a specific version, use it like below:
-#for i in `ls -d 9.1 9.0 8.4 8.3 8.2`
-for i in `ls -d 9.2`
+#for i in `ls -d 9.1 9.0 8.4 8.3 9.2`
+for i in `ls -d 9.1`
 do
 	cd $i
-	for j in `ls -d redhat fedora`
+#	for j in `ls -d redhat fedora`
 #	for j in `ls -d redhat`
-#	for j in `ls -d fedora`
+	for j in `ls -d fedora`
 	do
 		cd $j
-		for k in `ls -d *`
-#		for k in `ls -d rhel-6-*`
+#		for k in `ls -d *`
+#		for k in `ls -d rhel-5-*`
 # 		for k in `ls -d fedora-16* rhel-*`
-#		for k in `ls -d fedora-16*`
+		for k in `ls -d fedora-17-x*`
 		do
 			cd $k
 			echo "Working on $i - $j - $k"
 			# Create / update repo files and web interface:
 			createrepo -d . && repoview -u "http://yum.postgresql.org/srpms/$i/$j/$k/" -o repoview/ -t "PostgreSQL PGDG RPMs" .
 			# rsync files to the relevant directory:
-			rsync -ave ssh  --delete /media/disk/RPMS/srpms-web/$i/$j/$k  devrim@staging.pgrpms.org:/home/community/postgresqlrpms.org/public_html/srpms/$i/$j/
+			rsync -ave ssh  --delete /media/disk/RPMS/srpms-web/$i/$j/$k  devrim@www.gunduz.org:/home/community/postgresqlrpms.org/public_html/srpms/$i/$j/
 			cd ..
 		done
 		cd ..
 	done
 	cd ..
 done
-
