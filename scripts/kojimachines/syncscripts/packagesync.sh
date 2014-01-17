c628aa62b9be95deffcabb896d40832003176555
Devrim Gündüz <devrim@gunduz.org>
2014-01-17 19:28:38 +0000

    Bug fixes: * Add osdirarch variable, since it is different from real arch on newer releases. * Remove --checksum from createrepo -- it is obsoleted * Add -d to createrepo of SRPMs * Fix typo

--- scripts/kojimachines/syncscripts/packagesync.sh
+++ scripts/kojimachines/syncscripts/packagesync.sh
@@ -4,6 +4,7 @@ pgrelease=9.3
 osdistro=redhat
 os=rhel-6
 osarch=x86_64
+osdirarch=x86_64
 
 RPM_DIR=/var/lib/pgsql/rpm/ALLRPMS
 SRPM_DIR=/var/lib/pgsql/rpm/ALLSRPMS
@@ -15,15 +16,15 @@ mkdir -p $SRPM_DIR
 
 # rsync binary and source RPMs to their own directories:
 
-rsync -av --delete --stats /var/lib/pgsql/rpm/RPMS/$osarch/ /var/lib/pgsql/rpm/RPMS/noarch/ $RPM_DIR
+rsync -av --delete --stats /var/lib/pgsql/rpm/RPMS/$osdirarch/ /var/lib/pgsql/rpm/RPMS/noarch/ $RPM_DIR
 rsync -av --delete --stats /var/lib/pgsql/rpm/SRPMS/ /var/lib/pgsql/rpm/ALLSRPMS
 
 # Now, create repo for RPMs and SRPMS:
 
-createrepo --checksum=sha -g /usr/local/etc/postgresqldbserver-$pgrelease.xml -d --update $RPM_DIR && repoview -u "http://yum.postgresql.org/$pgrelease/$osdistro/$os-$osarch/" -o repoview/ -t "PostgreSQL PGDG $pgrelease RPMs" $RPM_DIR/
-createrepo --checksum=sha --update $SRPM_DIR && repoview -u "http://yum.postgresql.org/srpms/$pgrelease/$osdistro/$os-$osarch/" -o repoview/ -t "PostgreSQL PGDG $pgrelease RPMs" $SRPM_DIR/
+createrepo -g /usr/local/etc/postgresqldbserver-$pgrelease.xml -d --update $RPM_DIR && repoview -u "http://yum.postgresql.org/$pgrelease/$osdistro/$os-$osarch/" -o repoview/ -t "PostgreSQL PGDG $pgrelease RPMs" $RPM_DIR/
+createrepo -d --update $SRPM_DIR && repoview -u "http://yum.postgresql.org/srpms/$pgrelease/$osdistro/$os-$osarch/" -o repoview/ -t "PostgreSQL PGDG $pgrelease RPMs" $SRPM_DIR/
 
-# Finally, perform the rssync:
+# Finally, perform the rsync:
 rsync -ave ssh  --delete $RPM_DIR/  MASKED:/srv/yum/yum/$pgrelease/$osdistro/$os-$osarch
 rsync -ave ssh  --delete $SRPM_DIR/  MASKED:/srv/yum/yum/srpms/$pgrelease/$osdistro/$os-$osarch
 
