153e08c46dc98cabc33769b09ecc8321b7938472
Devrim Gündüz <devrim@gunduz.org>
2016-09-21 11:49:50 +0300

    Remove F-22 support from repository.

--- rpm/redhat/9.4/plruby/F-22/plruby--0.5.5.sql
+++
@@ -1,6 +0,0 @@
-CREATE FUNCTION plruby_call_handler () RETURNS language_handler
-	AS '/usr/lib64/ruby/vendor_ruby/plruby'
-        language C;
-
-CREATE OR REPLACE LANGUAGE plruby
-	HANDLER  plruby_call_handler;
