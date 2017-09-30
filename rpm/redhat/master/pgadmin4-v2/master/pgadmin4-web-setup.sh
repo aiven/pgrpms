84953af5468a60c22203b0eef2dec0408ee23d8f
Devrim Gündüz <devrim@gunduz.org>
2017-09-30 15:38:32 +0200

    pgadmin4-v2: Improve the new setup script: Now users can install Apache config file and reload/start httpd.

--- rpm/redhat/master/pgadmin4-v2/master/pgadmin4-web-setup.sh
+++ rpm/redhat/master/pgadmin4-v2/master/pgadmin4-web-setup.sh
@@ -16,5 +16,53 @@ fi
 # Own directories:
 chown apache: /var/log/pgadmin /var/lib/pgadmin -R
 
-exit 0
+echo "We can now configure the Apache Web server for you. This will create the pgAdmin4 conf file under /etc/httpd/conf.d/. Do you wish to continue?"
+select pgayn in "Yes" "No"; do
+    case $pgayn in
+        Yes )
+	echo
+	cp /etc/httpd/conf.d/pgadmin4-v2.conf.sample /etc/httpd/conf.d/pgadmin4-v2.conf
+	echo "pgAdmin4-v2 Apache config file is created as /etc/httpd/conf.d/pgadmin4-v2.conf"
+	break;;
+        No ) exit;;
+    esac
+done
 
+httpd_status=`ps cax | grep httpd`
+if [ $? -eq 0 ]; then
+	echo "Apache web server is running. Reload is required for pgAdmin4 installation to complete. Would you like to continue?"
+select pgahtyn in "Yes" "No"; do
+    case $pghtayn in
+        Yes )
+	echo
+	systemctl reload httpd
+	if [ $? != 0 ]
+	then
+		echo "Error reloading httpd. Please check systemd logs"
+	else
+		echo "Apache successfully reloaded. You can now start using pgAdmin4 in web mode"
+	fi
+        break;;
+        No ) exit;;
+    esac
+
+else
+	echo "Apache web server is not running. We can start the web server for you to finish pgAdmin4 installation. Would you like to continue?"
+select pgahtyn in "Yes" "No"; do
+    case $pghtayn in
+        Yes )
+	echo
+	systemctl restart httpd
+	if [ $? != 0 ]
+	then
+		echo "Error starting httpd. Please check systemd logs"
+	else
+		echo "Apache successfully started. You can now start using pgAdmin4 in web mode"
+	fi
+        break;;
+        No ) exit;;
+    esac
+
+fi
+
+exit 0
