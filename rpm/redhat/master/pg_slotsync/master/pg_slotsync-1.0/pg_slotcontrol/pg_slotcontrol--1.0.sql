1c478b8c94134a44b99b45ca4bc4a3657e0ed451
Devrim Gündüz <devrim@gunduz.org>
2020-02-10 17:39:45 +0300

    Hashtag BlameMagnus

--- rpm/redhat/master/pg_slotsync/master/pg_slotsync-1.0/pg_slotcontrol/pg_slotcontrol--1.0.sql
+++
@@ -1,9 +0,0 @@
--- complain if script is sourced in psql, rather than via CREATE EXTENSION
-\echo Use "CREATE EXTENSION pg_slotcontrol" to load this file. \quit
-
-CREATE FUNCTION pg_slotmove(text, pg_lsn)
-       RETURNS boolean
-       AS 'MODULE_PATHNAME', 'pg_slotmove'
-       LANGUAGE C VOLATILE STRICT;
-
-REVOKE ALL ON FUNCTION pg_slotmove(text, pg_lsn) FROM PUBLIC;
