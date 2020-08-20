aiven/pgrpms

commit 9345c241f4e8d8f2c0fb326bd02e8164fd4f2b16

SFCGAL: Add pkgconfig file to -devel subpackage, and remove no-longer-available .la file

rpm/redhat/master/common/sfcgal/master/sfcgal.spec
@@ -131,10 +131,10 @@ make %{?_smp_mflags} install/fast DESTDIR=%{buildroot}
 
 %files devel
 %{_includedir}/%{name}/
+%{_libdir}/pkgconfig/sfcgal.pc
 
 %files libs
 %{_libdir}/libSFCGAL.so*
-/usr/lib/libSFCGAL.la
 
 %changelog
 * Wed Aug 19 2020 Devrim Gündüz <devrim@gunduz.org> - 1.3.8-1

commit 32b212b9543c2686d46bb3f76df3d50c3f7cc275

sfcgal: Restore .la file for 1.3.1 builds, caused by 9345c24

rpm/redhat/master/common/sfcgal/master/sfcgal.spec
@@ -134,6 +134,9 @@ make %{?_smp_mflags} install/fast DESTDIR=%{buildroot}
 %if 0%{?fedora} || 0%{?rhel} >= 8 || 0%{?suse_version} >= 1315
 %{_libdir}/pkgconfig/sfcgal.pc
 %endif
+%if 0%{?rhel} && 0%{?rhel} <= 7
+/usr/lib/libSFCGAL.la
+%endif
 
 %files libs
 %{_libdir}/libSFCGAL.so*