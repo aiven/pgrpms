aiven/pgrpms

commit ba535ce9740f23ead60b001a43898bf3b4ffef8e

Use underscore before PostgreSQL version number for consistency, per https://www.postgresql.org/message-id/CAD%2BGXYMfbMnq3c-eYBRULC3nZ-W69uQ1ww8_0RQtJzoZZzp6ug%40mail.gmail.com
While passing, finish switching to pgdg-srpm-macros.

rpm/redhat/master/non-common/pgpool-II-41/master/pgpool-II.spec
@@ -25,9 +25,9 @@
 %endif
 
 Summary:		Pgpool is a connection pooling/replication server for PostgreSQL
-Name:			%{sname}-%{pgmajorversion}
+Name:			%{sname}_%{pgmajorversion}
 Version:		4.1.4
-Release:		1%{?dist}
+Release:		2%{?dist}
 License:		BSD
 URL:			http://pgpool.net
 Source0:		http://www.pgpool.net/mediawiki/images/%{sname}-%{version}.tar.gz
@@ -65,7 +65,9 @@ Requires(preun):	chkconfig
 Requires(preun):	initscripts
 Requires(postun):	initscripts
 %endif
+
 Obsoletes:		postgresql-pgpool < 1.0.0
+Obsoletes:		%{sname}-%{pgmajorversion} <= 4.1.4-1
 
 %ifarch ppc64 ppc64le
 %pgdg_set_ppc64le_min_requires
@@ -350,6 +352,10 @@ fi
 %{pginstdir}/lib/pgpool-regclass.so
 
 %changelog
+* Tue Oct 27 2020 Devrim Gündüz <devrim@gunduz.org> - 4.1.4-2
+- Use underscore before PostgreSQL version number for consistency, per:
+  https://www.postgresql.org/message-id/CAD%2BGXYMfbMnq3c-eYBRULC3nZ-W69uQ1ww8_0RQtJzoZZzp6ug%40mail.gmail.com
+
 * Thu Sep 17 2020 Devrim Gündüz <devrim@gunduz.org> 4.1.4-1
 - Update to 4.1.4
 

rpm/redhat/master/non-common/pgpool-II-35/master/pgpool-II.spec
@@ -24,9 +24,9 @@
 %global _varrundir %{_localstatedir}/run/%{name}
 
 Summary:		Pgpool is a connection pooling/replication server for PostgreSQL
-Name:			%{sname}-%{pgmajorversion}
+Name:			%{sname}_%{pgmajorversion}
 Version:		3.5.27
-Release:		1%{?dist}
+Release:		2%{?dist}
 License:		BSD
 URL:			http://pgpool.net
 Source0:		http://www.pgpool.net/mediawiki/images/%{sname}-%{version}.tar.gz
@@ -55,7 +55,9 @@ Requires(preun):	chkconfig
 Requires(preun):	initscripts
 Requires(postun):	initscripts
 %endif
+
 Obsoletes:		postgresql-pgpool < 1.0.0
+Obsoletes:		%{sname}-%{pgmajorversion} <= 3.5.27-1
 
 %ifarch ppc64 ppc64le
 %pgdg_set_ppc64le_min_requires
@@ -313,6 +315,10 @@ fi
 %{pginstdir}/lib/pgpool-regclass.so
 
 %changelog
+* Tue Oct 27 2020 Devrim Gündüz <devrim@gunduz.org> - 3.5.27-2
+- Use underscore before PostgreSQL version number for consistency, per:
+  https://www.postgresql.org/message-id/CAD%2BGXYMfbMnq3c-eYBRULC3nZ-W69uQ1ww8_0RQtJzoZZzp6ug%40mail.gmail.com
+
 * Thu Sep 17 2020 Devrim Gündüz <devrim@gunduz.org> - 3.5.27-1
 - Update to 3.5.27
 

rpm/redhat/master/non-common/pgpool-II-36/master/pgpool-II.spec
@@ -24,9 +24,9 @@
 %global _varrundir %{_localstatedir}/run/%{name}
 
 Summary:		Pgpool is a connection pooling/replication server for PostgreSQL
-Name:			%{sname}-%{pgmajorversion}
+Name:			%{sname}_%{pgmajorversion}
 Version:		3.6.23
-Release:		1%{?dist}
+Release:		2%{?dist}
 License:		BSD
 URL:			http://pgpool.net
 Source0:		http://www.pgpool.net/mediawiki/images/%{sname}-%{version}.tar.gz
@@ -57,7 +57,9 @@ Requires(preun):	chkconfig
 Requires(preun):	initscripts
 Requires(postun):	initscripts
 %endif
+
 Obsoletes:		postgresql-pgpool < 1.0.0
+Obsoletes:		%{sname}-%{pgmajorversion} <= 3.6.23-1
 
 %ifarch ppc64 ppc64le
 %pgdg_set_ppc64le_min_requires
@@ -325,6 +327,10 @@ fi
 %{pginstdir}/lib/pgpool-regclass.so
 
 %changelog
+* Tue Oct 27 2020 Devrim Gündüz <devrim@gunduz.org> - 3.6.23-2
+- Use underscore before PostgreSQL version number for consistency, per:
+  https://www.postgresql.org/message-id/CAD%2BGXYMfbMnq3c-eYBRULC3nZ-W69uQ1ww8_0RQtJzoZZzp6ug%40mail.gmail.com
+
 * Thu Sep 17 2020 Devrim Gündüz <devrim@gunduz.org> - 3.6.23-1
 - Update to 3.6.23
 

rpm/redhat/master/non-common/pgpool-II-37/master/pgpool-II.spec
@@ -24,9 +24,9 @@
 %global _varrundir %{_localstatedir}/run/%{name}
 
 Summary:		Pgpool is a connection pooling/replication server for PostgreSQL
-Name:			%{sname}-%{pgmajorversion}
+Name:			%{sname}_%{pgmajorversion}
 Version:		3.7.16
-Release:		1%{?dist}
+Release:		2%{?dist}
 License:		BSD
 URL:			http://pgpool.net
 Source0:		http://www.pgpool.net/mediawiki/images/%{sname}-%{version}.tar.gz
@@ -57,7 +57,9 @@ Requires(preun):	chkconfig
 Requires(preun):	initscripts
 Requires(postun):	initscripts
 %endif
+
 Obsoletes:		postgresql-pgpool < 1.0.0
+Obsoletes:		%{sname}-%{pgmajorversion} <= 3.7.16-1
 
 %ifarch ppc64 ppc64le
 %pgdg_set_ppc64le_min_requires
@@ -326,6 +328,10 @@ fi
 %{pginstdir}/lib/pgpool-regclass.so
 
 %changelog
+* Tue Oct 27 2020 Devrim Gündüz <devrim@gunduz.org> - 3.7.16-2
+- Use underscore before PostgreSQL version number for consistency, per:
+  https://www.postgresql.org/message-id/CAD%2BGXYMfbMnq3c-eYBRULC3nZ-W69uQ1ww8_0RQtJzoZZzp6ug%40mail.gmail.com
+
 * Thu Sep 17 2020 Devrim Gündüz <devrim@gunduz.org> - 3.7.16-1
 - Update to 3.7.16
 

rpm/redhat/master/non-common/pgpool-II-40/master/pgpool-II.spec
@@ -22,9 +22,9 @@
 %endif
 
 Summary:		Pgpool is a connection pooling/replication server for PostgreSQL
-Name:			%{sname}-%{pgmajorversion}
+Name:			%{sname}_%{pgmajorversion}
 Version:		4.0.11
-Release:		1%{?dist}
+Release:		2%{?dist}
 License:		BSD
 URL:			http://pgpool.net
 Source0:		http://www.pgpool.net/mediawiki/images/%{sname}-%{version}.tar.gz
@@ -58,7 +58,9 @@ Requires(preun):	chkconfig
 Requires(preun):	initscripts
 Requires(postun):	initscripts
 %endif
+
 Obsoletes:		postgresql-pgpool < 1.0.0
+Obsoletes:		%{sname}-%{pgmajorversion} <= 4.0.11-1
 
 %ifarch ppc64 ppc64le
 %pgdg_set_ppc64le_min_requires
@@ -345,6 +347,10 @@ fi
 %{pginstdir}/lib/pgpool-regclass.so
 
 %changelog
+* Tue Oct 27 2020 Devrim Gündüz <devrim@gunduz.org> - 4.0.11-2
+- Use underscore before PostgreSQL version number for consistency, per:
+  https://www.postgresql.org/message-id/CAD%2BGXYMfbMnq3c-eYBRULC3nZ-W69uQ1ww8_0RQtJzoZZzp6ug%40mail.gmail.com
+
 * Thu Sep 17 2020 Devrim Gündüz <devrim@gunduz.org> - 4.0.11-1
 - Update to 4.0.11
 

commit 8f4bcf16d989ac2cfb430705c86b0e084f45d886

pgpool-II-41: Invent new %%unitname macro to fix unit file breakage caused by ba535ce9740f23ead60b001a43898bf3b4ffef8e .

rpm/redhat/master/non-common/pgpool-II-41/master/pgpool-II.spec
@@ -1,5 +1,6 @@
 %global pgpoolinstdir /usr/pgpool-%{pgpackageversion}
 %global sname pgpool-II
+%global unitname	%{sname}-%{pgmajorversion}
 
 %if 0%{?rhel} && 0%{?rhel} <= 6
 %global systemd_enabled 0
@@ -27,7 +28,7 @@
 Summary:		Pgpool is a connection pooling/replication server for PostgreSQL
 Name:			%{sname}_%{pgmajorversion}
 Version:		4.1.5
-Release:		2%{?dist}
+Release:		3%{?dist}
 License:		BSD
 URL:			http://pgpool.net
 Source0:		http://www.pgpool.net/mediawiki/images/%{sname}-%{version}.tar.gz
@@ -161,7 +162,7 @@ USE_PGXS=1 %{__make} %{?_smp_mflags} -C src/sql/pgpool-regclass
 
 %if %{systemd_enabled}
 %{__install} -d %{buildroot}%{_unitdir}
-%{__install} -m 755 %{SOURCE1} %{buildroot}%{_unitdir}/%{sname}-%{pgmajorversion}.service
+%{__install} -m 755 %{SOURCE1} %{buildroot}%{_unitdir}/%{unitname}.service
 
 # ... and make a tmpfiles script to recreate it at reboot.
 %{__mkdir} -p %{buildroot}%{_tmpfilesdir}
@@ -314,7 +315,7 @@ fi
 %if %{systemd_enabled}
 %ghost %{_rundir}
 %{_tmpfilesdir}/%{name}.conf
-%{_unitdir}/%{sname}-%{pgmajorversion}.service
+%{_unitdir}/%{unitname}.service
 %else
 %{_sysconfdir}/init.d/%{name}
 %endif
@@ -354,6 +355,10 @@ fi
 %{pginstdir}/lib/pgpool-regclass.so
 
 %changelog
+* Wed Dec 9 2020 Devrim Gündüz <devrim@gunduz.org> - 4.1.5-3
+- Invent new %%unitname macro to fix unit file breakage
+  caused by ba535ce9740f23ead60b001a43898bf3b4ffef8e .
+
 * Wed Nov 25 2020 Devrim Gündüz <devrim@gunduz.org> - 4.1.5-2
 - Also obsolete subpackages.
 