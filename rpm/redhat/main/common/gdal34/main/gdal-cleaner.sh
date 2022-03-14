e4d7ce3619cdbb66b475a5566dcb8e268a31ee8e
Devrim Gündüz <devrim@gunduz.org>
2022-03-14 15:19:51 +0000

    gdal34: Update gdal-cleaner for 3.4.2, and also move some logic in the spec file to here to avoid losing time during each build.

--- rpm/redhat/main/common/gdal34/main/gdal-cleaner.sh
+++ rpm/redhat/main/common/gdal34/main/gdal-cleaner.sh
@@ -1,21 +1,43 @@
 #!/bin/bash
-VERSION="3.4.1"
+VERSION="3.4.2"
 
-tar zxf gdal-"${VERSION}".tar.gz
+rm -rf gdal-"${VERSION}"{,-fedora}
+tar zxf v"${VERSION}".tar.gz
 
-mv gdal-"${VERSION}"{,-fedora} && pushd gdal-"${VERSION}"-fedora/data
+mv gdal-"${VERSION}"{,-fedora} && pushd gdal-"${VERSION}"-fedora/gdal/data
 
-rm data/cubewerx_extra.wkt
-rm data/esri_extra.wkt
-rm data/esri_Wisconsin_extra.wkt
-rm data/esri_StatePlane_extra.wkt
-rm data/ecw_cs.wkt
+rm cubewerx_extra.wkt
+rm esri_StatePlane_extra.wkt
+rm ecw_cs.wkt
 
-#Really necessary?
-rm -r swig/php
+# Sanitize linebreaks and encoding
+# /frmts/grib/degrib18/degrib/metaname.cpp
+# and geoconcept.c are potentially dangerous to change
+
+set +x
+for f in `find . -type f` ; do
+  if file $f | grep -q ISO-8859 ; then
+    set -x
+    iconv -f ISO-8859-1 -t UTF-8 $f > ${f}.tmp && \
+      mv -f ${f}.tmp $f
+    set +x
+  fi
+  if file $f | grep -q CRLF ; then
+    set -x
+    sed -i -e 's|\r||g' $f
+    set +x
+  fi
+done
+set -x
+
+# Do more cleanup:
+for f in apps; do
+pushd $f
+  chmod 644 *.cpp
+popd
+done
 
 popd
-
 
 #TODO: Insert Provenance file
 
