aiven/pgrpms

commit feb24403572f8a79abb82b7ce179952cd93182df

pgdg-yum/Fedora: Fix bogus changelog date

rpm/redhat/main/common/pgdg-yum/Fedora-main/pgdg-fedora.spec
@@ -35,7 +35,7 @@ key for PGDG RPMs.
 %{_sysconfdir}/pki/rpm-gpg/*
 
 %changelog
-* Mon Mar 5 2024 Devrim Gündüz <devrim@gunduz.org> - 42.0-32PGDG
+* Wed Apr 10 Devrim Gündüz <devrim@gunduz.org> - 42.0-32PGDG
 - Introduce debuginfo repo for the common RPMs
 
 * Mon Jan 8 2024 Devrim Gündüz <devrim@gunduz.org> - 42.0-31PGDG

commit bcc6473cfd28a3498889cab69aa53e39ab25e599

pgdg-yum/Fedora: Fix another changelog date issue introduced in feb2440357

rpm/redhat/main/common/pgdg-yum/Fedora-main/pgdg-fedora.spec
@@ -35,7 +35,7 @@ key for PGDG RPMs.
 %{_sysconfdir}/pki/rpm-gpg/*
 
 %changelog
-* Wed Apr 10 Devrim Gündüz <devrim@gunduz.org> - 42.0-32PGDG
+* Wed Apr 10 2024 Devrim Gündüz <devrim@gunduz.org> - 42.0-32PGDG
 - Introduce debuginfo repo for the common RPMs
 
 * Mon Jan 8 2024 Devrim Gündüz <devrim@gunduz.org> - 42.0-31PGDG