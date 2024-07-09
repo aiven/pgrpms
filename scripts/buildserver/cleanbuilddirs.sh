b90ae7e923739c1306c501a45c987fd77428b684
Devrim Gunduz <devrim@gunduz.org>
2024-07-09 11:07:19 +0300

    scripts: Add script used to clean up build directories to save disk space on build instances

---
+++ scripts/buildserver/cleanbuilddirs.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/bash
+
+#########################################################
+#							#
+# Devrim Gündüz <devrim@gunduz.org> - 2024		#
+#							#
+#########################################################
+
+# Include common values:
+source ~/bin/global.sh
+
+# Make sure only postgres user can run this script:
+if [ "$(id -u)" != "26" ]; then
+	clear
+	echo
+	echo "${red}ERROR:${reset} This script must be run as postgres user" 1>&2
+	echo
+	exit 1
+fi
+
+# First clean common directory:
+rm -rf ~/rpmcommon/BUILD/*
+rm -rf ~/rpmcommon/BUILDROOT/*
+
+rm -rf /var/lib/pgsql/rpm${pgAlphaVersion}testing/BUILD/*
+rm -rf /var/lib/pgsql/rpm${pgAlphaVersion}testing/BUILDROOT/*
+
+# Now clean testing dirs:
+for packageCleanVersion in ${pgTestBuilds[@]}
+do
+	rm -rf /var/lib/pgsql/rpm${packageCleanVersion}testing/BUILD/*
+	rm -rf /var/lib/pgsql/rpm${packageCleanVersion}testing/BUILDROOT/*
+done
+
+# Finally, delete stable dirs:
+for packageCleanVersion in ${pgStableBuilds[@]}
+do
+	rm -rf /var/lib/pgsql/rpm${packageCleanVersion}/BUILD/*
+	rm -rf /var/lib/pgsql/rpm${packageCleanVersion}/BUILDROOT/*
+done
+
