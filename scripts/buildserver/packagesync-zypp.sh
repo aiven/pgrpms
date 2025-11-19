70cc2d5b7ed30b52905e511757a0eeca9ed25dd6
Devrim Gunduz <devrim@gunduz.org>
2025-11-19 14:41:23 +0300

    scripts: Add zypp sync scripts (which are already in prod anyway). Also use the osminversion variable that I committed in ccfdd766aa to support multiple SLES service packs (we do that anyway but the infrastructure was a bit different)

---
+++ scripts/buildserver/packagesync-zypp.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/bash
+
+#########################################################
+#							#
+# Devrim Gündüz <devrim@gunduz.org> - 2024		#
+#							#
+#########################################################
+
+# Include common values:
+source ~/bin/global.sh
+
+if [ "$1" == "" ]
+then
+	:
+else
+	if [[ "${pgStableBuilds[@]}" =~ "$1" ]]
+	then
+		declare -a pgStableBuilds=("$1")
+	else
+		echo "${red}ERROR:${reset} PostgreSQL version $1 is not supported."
+		exit 1
+		fi
+fi
+
+for packageSyncVersion in ${pgStableBuilds[@]}
+do
+	export BASE_DIR=/var/lib/pgsql/rpm${packageSyncVersion}
+
+	export RPM_DIR=$BASE_DIR/ALLRPMS
+	export DEBUG_RPM_DIR=$BASE_DIR/ALLDEBUGRPMS
+	export SRPM_DIR=$BASE_DIR/ALLSRPMS
+
+	# Create directories for binary and source RPMs. This directory will help us
+	# to create the repo files easily:
+	mkdir -p $RPM_DIR
+	mkdir -p $SRPM_DIR
+	mkdir -p $DEBUG_RPM_DIR
+
+	# rsync binary and source RPMs to their own directories:
+	echo "${green}Syncing PostgreSQL $packageSyncVersion RPMs${reset}"
+
+	rsync --checksum -av --delete --stats $BASE_DIR/RPMS/$osarch/ $BASE_DIR/RPMS/noarch/ $RPM_DIR
+	rsync --checksum -av --delete --stats $BASE_DIR/SRPMS/ $SRPM_DIR
+
+	# Move debuginfo and debugsource packages to a separate directory.
+	# First clean the old ones, and then copy existing ones:
+	rm -rf $DEBUG_RPM_DIR/*
+	mv $RPM_DIR/*debuginfo* $RPM_DIR/*debugsource* $DEBUG_RPM_DIR/
+
+	# Now, create repo for RPMs and SRPMS:
+
+	createrepo --changelog-limit=3 --workers=4 -g /usr/local/etc/postgresqldbserver-$packageSyncVersion.xml -d --update $RPM_DIR
+	createrepo --changelog-limit=3 --workers=4 -g /usr/local/etc/postgresqldbserver-$packageSyncVersion.xml -d --update $DEBUG_RPM_DIR
+	createrepo --changelog-limit=3 --workers=4 -d --update $SRPM_DIR
+
+	echo $GPG_PASSWORD | /usr/bin/gpg2 -a --pinentry-mode loopback --detach-sign --batch --yes --passphrase-fd 0 $RPM_DIR/repodata/repomd.xml
+	echo $GPG_PASSWORD | /usr/bin/gpg2 -a --pinentry-mode loopback --detach-sign --batch --yes --passphrase-fd 0 $DEBUG_RPM_DIR/repodata/repomd.xml
+	echo $GPG_PASSWORD | /usr/bin/gpg2 -a --pinentry-mode loopback --detach-sign --batch --yes --passphrase-fd 0 $SRPM_DIR/repodata/repomd.xml
+
+	# We currently sync only x86_64 packages to yonada. The rest is pulled from yonada:
+	if [ "$osarch" = "x86_64" ]
+	then
+		# Finally, perform the rsync:
+		rsync --checksum -ave ssh --delete $RPM_DIR/ yumupload@yum.postgresql.org:zypp/zypp/$packageSyncVersion/$osdistro/$os.$osminversion-$osarch
+	fi
+
+	# Sync SRPMs to S3 bucket:
+	aws s3 sync $SRPM_DIR s3://zypp-srpms.postgresql.org20250618120322107700000001/srpms/$packageSyncVersion/$osdistro/$os.$osminversion-$osarch --exclude "*.html" --exclude "repodata"
+	aws s3 sync --delete $SRPM_DIR s3://zypp-srpms.postgresql.org20250618120322107700000001/srpms/$packageSyncVersion/$osdistro/$os.$osminversion-$osarch/repodata/ --exclude "*.html"
+	aws cloudfront create-invalidation --distribution-id $CF_SRPM_DISTRO_ID --path /srpms/$packageSyncVersion/$osdistro/$os-$osarch/repodata/*
+
+	# Sync debug* RPMs to S3 bucket:
+	aws s3 sync $DEBUG_RPM_DIR s3://zypp-debuginfo.postgresql.org20250312201116651400000002/debug/$packageSyncVersion/$osdistro/$os.$osminversion-$osarch/ --exclude "*.html" --exclude "repodata"
+	aws s3 sync --delete $DEBUG_RPM_DIR s3://zypp-debuginfo.postgresql.org20250312201116651400000002/debug/$packageSyncVersion/$osdistro/$os.$osminversion-$osarch/repodata/ --exclude "*.html"
+	aws cloudfront create-invalidation --distribution-id $CF_DEBUG_DISTRO_ID --path /debug/$packageSyncVersion/$osdistro/$os-$osarch/repodata/*
+done
+
+exit 0
+
+
+
---
+++ scripts/buildserver/packagesynccommon-zypp.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/bash
+
+#########################################################
+#							#
+# Devrim Gündüz <devrim@gunduz.org> - 2024		#
+#							#
+#########################################################
+
+# Include common values:
+source ~/bin/global.sh
+
+export COMMON_RPM_DIR=/var/lib/pgsql/rpmcommon/ALLRPMS
+export COMMON_SRPM_DIR=/var/lib/pgsql/rpmcommon/ALLSRPMS
+export COMMON_DEBUG_RPM_DIR=/var/lib/pgsql/rpmcommon/ALLDEBUGRPMS
+
+# Create directories for binary and source RPMs. This directory will help us
+# to create the repo files easily (though this can be done via ansible during initial installation:
+
+mkdir -p $COMMON_RPM_DIR
+mkdir -p $COMMON_SRPM_DIR
+mkdir -p $COMMON_DEBUG_RPM_DIR
+
+echo "${green}Syncing PostgreSQL common RPMs for $os - $osarch${reset}"
+
+# rsync binary and source RPMs to their own directories:
+
+rsync --checksum -av --delete --stats /var/lib/pgsql/rpmcommon/RPMS/$osarch/ /var/lib/pgsql/rpmcommon/RPMS/noarch/ $COMMON_RPM_DIR
+rsync --checksum -av --delete --stats /var/lib/pgsql/rpmcommon/SRPMS/ $COMMON_SRPM_DIR
+
+# Move debuginfo and debugsource packages to a separate directory.
+# First clean the old ones, and then copy existing ones:
+rm -rf $COMMON_DEBUG_RPM_DIR/*
+mv $COMMON_RPM_DIR/*debuginfo* $COMMON_RPM_DIR/*debugsource* $COMMON_DEBUG_RPM_DIR/
+
+# Now, create repo for RPMs and SRPMS:
+
+createrepo --changelog-limit=3 --workers=4 -d --update $COMMON_RPM_DIR
+createrepo --changelog-limit=3 --workers=4 -d --update $COMMON_SRPM_DIR
+createrepo --changelog-limit=3 --workers=4 -d --update $COMMON_DEBUG_RPM_DIR
+
+echo $GPG_PASSWORD | /usr/bin/gpg2 -a --pinentry-mode loopback --detach-sign --batch --yes --passphrase-fd 0 $COMMON_RPM_DIR/repodata/repomd.xml
+echo $GPG_PASSWORD | /usr/bin/gpg2 -a --pinentry-mode loopback --detach-sign --batch --yes --passphrase-fd 0 $COMMON_SRPM_DIR/repodata/repomd.xml
+echo $GPG_PASSWORD | /usr/bin/gpg2 -a --pinentry-mode loopback --detach-sign --batch --yes --passphrase-fd 0 $COMMON_DEBUG_RPM_DIR/repodata/repomd.xml
+
+# We currently pull packages from yonada, so skip the next line:
+rsync --checksum -ave ssh --delete $COMMON_RPM_DIR/ yumupload@yum.postgresql.org:zypp/zypp/common/$osdistro/$os-$osarch
+
+# Sync SRPMs to S3 bucket:
+aws s3 sync $COMMON_SRPM_DIR s3://zypp-srpms.postgresql.org20250618120322107700000001/srpms/common/$osdistro/$os.$osminversion-$osarch/ --exclude "*.html" --exclude "repodata"
+aws s3 sync --delete $COMMON_SRPM_DIR/repodata/ s3://zypp-srpms.postgresql.org20250618120322107700000001/srpms/common/$packageSyncVersion/$osdistro/$os.$osminversion-$osarch/repodata/ --exclude "*.html"
+aws cloudfront create-invalidation --distribution-id $CF_SRPM_DISTRO_ID --path /srpms/common/$packageSyncVersion/$osdistro/$os.$osminversion-$osarch/repodata/*
+
+# Sync debug* RPMs to S3 bucket:
+aws s3 sync $COMMON_DEBUG_RPM_DIR s3://zypp-debuginfo.postgresql.org20250312201116651400000002/debug/common/$osdistro/$os.$osminversion-$osarch/ --exclude "*.html" --exclude "repodata"
+aws s3 sync --delete $COMMON_DEBUG_RPM_DIR/repodata/ s3://zypp-debuginfo.postgresql.org20250312201116651400000002/debug/common/$packageSyncVersion/$osdistro/$os.$osminversion-$osarch/repodata/ --exclude "*.html"
+aws cloudfront create-invalidation --distribution-id $CF_DEBUG_DISTRO_ID --path /debug/common/$packageSyncVersion/$osdistro/$os.$osminversion-$osarch/repodata/*
+
+exit 0
