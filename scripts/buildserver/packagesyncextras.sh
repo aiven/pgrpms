5b3625e568efee6a5347347ab0ca3613cdc3a684
Devrim Gunduz <devrim@gunduz.org>
2026-01-14 13:05:58 +0300

    scripts: Update script like I did in 23b907859ebbec1

--- scripts/buildserver/packagesyncextras.sh
+++ scripts/buildserver/packagesyncextras.sh
@@ -50,7 +50,7 @@ echo $GPG_PASSWORD | /usr/bin/gpg2 -a --pinentry-mode loopback --detach-sign --b
 echo $GPG_PASSWORD | /usr/bin/gpg2 -a --pinentry-mode loopback --detach-sign --batch --yes --passphrase-fd 0 $EXTRAS_SRPM_DIR/repodata/repomd.xml
 echo $GPG_PASSWORD | /usr/bin/gpg2 -a --pinentry-mode loopback --detach-sign --batch --yes --passphrase-fd 0 $EXTRAS_DEBUG_RPM_DIR/repodata/repomd.xml
 
-#  We currently pull packages from yonada, so skip the next line:
+# We currently pull packages from yonada, so skip the next line:
 # rsync -ave ssh --delete $EXTRAS_RPM_DIR/ yumupload@yum.postgresql.org:yum/yum/extras/$osdistro/$os.$osminversion-$osarch
 
 # Sync SRPMs to S3 bucket:
@@ -63,5 +63,16 @@ aws s3 sync $EXTRAS_DEBUG_RPM_DIR $awsdebuginfourl/debug/extras/$osdistro/$os.$o
 aws s3 sync --delete $EXTRAS_DEBUG_RPM_DIR/repodata/ $awsdebuginfourl/debug/extras/$osdistro/$os.$osminversion-$osarch/repodata/ --exclude "*.html"
 aws cloudfront create-invalidation --distribution-id $CF_DEBUG_DISTRO_ID --path /debug/extras/$osdistro/$os.$osminversion-$osarch/repodata/*
 
+# S3 does not allow symlinks, so we have to sync the packages once again to the OS major version directory if this is the latest version of the OS:
+if [ "$osislatest" == 1 ]
+then
+	aws s3 sync $EXTRAS_SRPM_DIR $awssrpmurl/srpms/extras/$osdistro/$os-$osarch --exclude "*.html" --exclude "repodata"
+	aws s3 sync --delete $EXTRAS_SRPM_DIR/repodata/ $awssrpmurl/srpms/extras/$osdistro/$os-$osarch/repodata/ --exclude "*.html"
 
+	aws s3 sync $EXTRAS_DEBUG_RPM_DIR $awsdebuginfourl/debug/extras/$osdistro/$os-$osarch/ --exclude "*.html"
+	aws s3 sync --delete $EXTRAS_DEBUG_RPM_DIR/repodata/ $awsdebuginfourl/debug/extras/$osdistro/$os-$osarch/repodata/ --exclude "*.html"
+	# Invalidate the caches:
+	aws cloudfront create-invalidation --distribution-id $CF_SRPM_DISTRO_ID --path /srpms/extras/$packageSyncVersion/$osdistro/$os-$osarch/repodata/*
+	aws cloudfront create-invalidation --distribution-id $CF_DEBUG_DISTRO_ID --path /debug/extras/$packageSyncVersion/$osdistro/$os-$osarch/repodata/*
+fi
 exit 0
