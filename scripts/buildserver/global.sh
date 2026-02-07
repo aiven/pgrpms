9917bede76befbb6a698b87ba59c7e7d8b8276f9
Devrim Gunduz <devrim@gunduz.org>
2026-02-07 15:28:01 +0300

    Review global.sh: - Replace all backticks with $(...) syntax - Properly quoted variables in exports and the function - Group related exports with comments - Use consistent () for empty array - Remove unnecessary braces in xargs and find commands

--- scripts/buildserver/global.sh
+++ scripts/buildserver/global.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
 #########################################################
-#							#
-# Devrim Gündüz <devrim@gunduz.org> - 2026		#
-#							#
+#                                                       #
+# Devrim Gündüz <devrim@gunduz.org> - 2026            #
+#                                                       #
 #########################################################
 
 # Make sure only postgres user can run this script:
@@ -16,46 +16,53 @@ if [ "$(id -u)" != "26" ]; then
 fi
 
 # Color schemes
-red=`tput setaf 1`
-green=`tput setaf 2`
-blue=`tput setaf 4`
-reset=`tput sgr0`
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+blue=$(tput setaf 4)
+reset=$(tput sgr0)
 
-export osmajorversion=10	# Major version: 10, 9, 15, 43
-export os=rhel-${osmajorversion}		# rhel-9, sles-15, fedora-43
-export osminversion=1		# Will be used to support multiple OS minor versions like SLES 15.7, RHEL 10.1
-export osislatest=0		# Is this the latest minor version of the OS or not? 1 or 0
-export osarch=x86_64		# x86_64, aarch64, ppc64le
-export osdistro=redhat		# fedora, redhat, suse
-export git_os=EL-${osmajorversion}		# EL-9, F-43, SLES-15
-export extrasrepoenabled=1	# 1 or 0. Currently for RHEL and SLES.
+# OS Configuration
+export osmajorversion=10			# Major version: 10, 9, 15, 43
+export os="rhel-${osmajorversion}"		# rhel-9, sles-15, fedora-43
+export osminversion=1				# Will be used to support multiple OS minor versions like SLES 15.7, RHEL 10.1
+export osislatest=0				# Is this the latest minor version of the OS or not? 1 or 0
+export osarch=x86_64				# x86_64, aarch64, ppc64le
+export osdistro=redhat				# fedora, redhat, suse
+export git_os="EL-${osmajorversion}"		# EL-9, F-43, SLES-15
+export extrasrepoenabled=1			# 1 or 0. Currently for RHEL and SLES.
 
+# GPG Configuration
 export GPG_TTY=$(tty)
 export GPG_PASSWORD=foobar
 
+# AWS Configuration
 export AWS_PAGER=""
-export awssrpmurl=s3://dnf-srpms.postgresql.org20250313103537584600000001		# s3://dnf-srpms.postgresql.org20250313103537584600000001 or s3://zypp-srpms.postgresql.org20250618120322107700000001
-export awsdebuginfourl=s3://dnf-debuginfo.postgresql.org20250312201116649700000001	# s3://dnf-debuginfo.postgresql.org20250312201116649700000001 or s3://zypp-debuginfo.postgresql.org20250312201116651400000002
+export awssrpmurl="s3://dnf-srpms.postgresql.org20250313103537584600000001"		# s3://dnf-srpms.postgresql.org20250313103537584600000001 or s3://zypp-srpms.postgresql.org20250618120322107700000001
+export awsdebuginfourl="s3://dnf-debuginfo.postgresql.org20250312201116649700000001"	# s3://dnf-debuginfo.postgresql.org20250312201116649700000001 or s3://zypp-debuginfo.postgresql.org20250312201116651400000002
 
+# CloudFront Configuration
 export CF_DEBUG_DISTRO_ID=XXXXXXXXXXXXXXXXXX
 export CF_SRPM_DISTRO_ID=XXXXXXXXXXXXXXXXXX
 
-declare -a pgStableBuilds=("18 17 16 15 14" )
-declare -a pgTestBuilds=("18 17 16 15 14" )
-declare -a pgBetaVersion=
-declare -a pgAlphaVersion=19
+# PostgreSQL Build Versions
+declare -a pgStableBuilds=("18 17 16 15 14")
+declare -a pgTestBuilds=("18 17 16 15 14")
+declare -a pgBetaVersion=()
+declare -a pgAlphaVersion=(19)
 
 # Common function to sign the package.
-sign_package(){
+sign_package() {
 	# Remove all files with .sig suffix. They are leftovers which appear
 	# when signing process is not completed. Signing will be broken when
 	# they exist.
-	find ~/rpm* pgdg* -iname "*.sig" -print0 | xargs -0 /bin/rm -v -rf "{}"
+	find ~/rpm* pgdg* -iname "*.sig" -print0 | xargs -0 /bin/rm -v -rf
 
 	# Remove all buildreqs.nosrc packages:
-	find ~/rpm* pgdg* -iname "*buildreqs.nosrc*" -print0 | xargs -0 /bin/rm -v -rf "{}"
+	find ~/rpm* pgdg* -iname "*buildreqs.nosrc*" -print0 | xargs -0 /bin/rm -v -rf
 
 	# Find the packages, and sign them. Using an expect script to automate signing process.
 	# The first parameter refers to the location of the RPMs:
-	for signpackagelist in `find ~/$1* -iname "*$signPackageName*$packageVersion*.rpm"`; do /usr/bin/expect ~/bin/signrpms.expect $signpackagelist; done
+	for signpackagelist in $(find ~/"$1"* -iname "*${signPackageName}*${packageVersion}*.rpm"); do
+		/usr/bin/expect ~/bin/signrpms.expect "$signpackagelist"
+	done
 }
